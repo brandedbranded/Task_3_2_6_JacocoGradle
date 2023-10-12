0bef68e4421e631727a40ee44dc09e1b0e2a2023
brandedbranded <128071703+brandedbranded@users.noreply.github.com>
2023-10-12 15:06:17 +0300

    Билд, упавший из-за недостаточного покрытия

--- src/test/java/org/example/BoundaryServiceTest.java
+++ src/test/java/org/example/BoundaryServiceTest.java
@@ -13,7 +13,7 @@ class BoundaryServiceTest {
     private final BoundaryService boundaryService = new BoundaryService();
 
     private static Stream<int[]> arrays() {
-        return Stream.of((new int[]{2, 4, 0}), (new int[]{3, 3, 3}), (new int[]{4, 4, 6}), (new int[]{-8, 1, 3}));
+        return Stream.of(/*(new int[]{2, 4, 0}), (new int[]{3, 3, 3}), (new int[]{4, 4, 6}), */(new int[]{3, 3, 3}));
     }
 
     @ParameterizedTest
